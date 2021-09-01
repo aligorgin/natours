fcccf784466a91c53e87fa3312765f6a2ebbb286
ali <aliwolfi18@gmail.com>
2021-08-31 19:52:54 -0700

    added animated botton

--- src/ui/natours/Header.tsx
+++ src/ui/natours/Header.tsx
@@ -9,6 +9,8 @@ export default function Header() {
         clipPath: 'polygon(0 0, 100% 0, 100% 75vh, 0 100%)',
     };
 
+    const styleOfA = 'transition-all duration-200 transform  hover:-translate-y-2 rounded-full uppercase py-[15px] px-[40px] inline-block bg-white text-[#777] relative active:-translate-y-1 hover:shadow-light active:shadow-lessLight after:inline-block after:h-full after:w-full after:rounded-full after:absolute after:bg-white after:top-0 after:left-0 after:-z-1 after:transform hover:after:scale-x-140 hover:after:scale-y-160 hover:after:opacity-0 hover:after:transition-all hover:after:duration-400 animate-moveInBottom'
+
     return (
         <div style={style}
              className='relative h-[95vh] bg-gradient-to-br from-[rgba(126,213,111,0.8)] to-[rgba(40,180,131,0.8)]'>
@@ -21,11 +23,16 @@ export default function Header() {
             </div>
 
             <div style={{backfaceVisibility: 'hidden'}}
-                 className='absolute top-[40%] left-1/2 transform -translate-y-1/2 -translate-x-1/2'>
-                <h1 className='text-gray-100 uppercase'>
+                 className='absolute top-[40%] left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center'>
+                <h1 className='text-gray-100 uppercase mb-[60px]'>
                     <span className='animate-fadeRight block tracking-[35px] text-[60px] font-normal'>outdoors</span>
                     <span className='animate-fadeLeft block tracking-[17.4px] text-[20px] font-bold'>is where life happens</span>
                 </h1>
+                <a href="#"
+                   style={{animationFillMode: 'backwards'}}
+                   className={styleOfA}>
+                    discover our tours
+                </a>
             </div>
 
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,27 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
+            scale: {
+                '140': '1.4',
+                '160': '1.6'
+            },
+            zIndex: {
+                '-1': '-1',
+            },
+            boxShadow: {
+                light: '0 10px 20px rgba(0,0,0,.2)',
+                lessLight: '0 5px 10px rgba(0,0,0,.2)'
+            },
+            translate: {
+                '0.75': '3px'
+            },
             fontFamily: {
                 'lato': ['lato']
             },
             animation: {
                 fadeLeft: 'fadeLeft 1s ease-out',
                 fadeRight: 'fadeRight 1s ease-out',
+                moveInBottom: 'moveInBottom .5s ease-out .75s'
             },
             keyframes: {
                 fadeRight: {
@@ -17,11 +32,15 @@ module.exports = {
                     '80%': {transform: 'translateX(20px)'},
                     '100%': {opacity: '1', transform: 'translate(0)'},
                 },
-                fadeLeft:{
+                fadeLeft: {
                     '0%': {opacity: '0', transform: 'translateX(100px)'},
                     '80%': {transform: 'translateX(-20px)'},
                     '100%': {opacity: '1', transform: 'translate(0)'},
-                }
+                },
+                moveInBottom: {
+                    '0%': {opacity: '0', transform: 'translateY(30px)'},
+                    '100%': {opacity: '1', transform: 'translate(0)'},
+                },
             }
         },
     },
