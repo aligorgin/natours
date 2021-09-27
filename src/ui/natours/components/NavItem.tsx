66c7f62cb0412c4b765b725279732171027272de
ali <aliwolfi18@gmail.com>
2021-09-27 13:56:51 +0330

    still working on nav

--- src/ui/natours/components/NavItem.tsx
+++ src/ui/natours/components/NavItem.tsx
@@ -1,13 +1,14 @@
 interface Props {
     text: string;
+    num:string
 }
 
-export default function NavItem({text}: Props) {
+export default function NavItem({text,num}: Props) {
     return (
         <>
-            <li>
-                <a href="#">
-                    {text}
+            <li className='m-[1rem]'>
+                <a href="#" style={{backgroundSize:'222%'}} className='inline-block hover:translate-x-[1rem] hover:text-primary py-[1rem] px-[2rem] text-[3rem] font-light text-white uppercase nav-text-hover transition-all duration-300'>
+                    <span className='mr-[1.5rem] inline-block'>{num}</span>{text}
                 </a>
             </li>
         </>
--- src/ui/natours/containers/Nav.tsx
+++ src/ui/natours/containers/Nav.tsx
@@ -1,21 +1,27 @@
 import NavItem from "../components/NavItem";
+import {useState} from "react";
+import clsx from "clsx";
 
 export default function Nav() {
+    const [isClose, setClose] = useState(true);
+
+
     return (
         <div>
-            <div>
-                front
+            <div onClick={()=>setClose((prevState => !prevState))}
+                className={clsx('bg-white h-[7rem] w-[7rem] fixed top-[6rem] right-[6rem] rounded-full z-50')}>
             </div>
-            <div className='h-[6rem] w-[6rem] rounded-full fixed right-[6.5rem] top-[6.5rem] radial-gradient-green hover:scale-[50] transition-all duration-500 z-50'>
-
-            </div>
-            <nav>
-                <ul>
-                    <NavItem text={'about natous'}/>
-                    <NavItem text={'your benfits'}/>
-                    <NavItem text={'popular tours'}/>
-                    <NavItem text={'Stories'}/>
-                    <NavItem text={'book now'}/>
+            <div
+                onClick={() => setClose((prevState => !prevState))}
+                className={clsx('h-[6rem] w-[6rem] rounded-full fixed right-[6.5rem] top-[6.5rem] radial-gradient-green z-30 scale-[80]',
+                    isClose ? '' : '')}/>
+            <nav className='h-screen w-full fixed top-0 left-0 z-40 flex justify-center items-center'>
+                <ul className='text-center'>
+                    <NavItem num={'01'} text={'about natous'}/>
+                    <NavItem num={'02'} text={'your benefits'}/>
+                    <NavItem num={'03'} text={'popular tours'}/>
+                    <NavItem num={'04'} text={'Stories'}/>
+                    <NavItem num={'06'} text={'book now'}/>
                 </ul>
             </nav>
         </div>
